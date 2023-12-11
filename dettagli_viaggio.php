8999b0b0ab1b5fa607f11c8a09b6de94989237ba
Melissa <melissa.prencisvalle@iismajoranamoncalieri.edu.it>
2023-12-11 11:40:21 +0100

    inizio esercizio

---
+++ dettagli_viaggio.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="utf-8">
+    <title>Dettagli viaggio</title>
+</head>
+<body>
+    <h1>Dettagli viaggio</h1>
+<?php 
+session_start();
+$nome =$_POST["nome"];
+$cognome =$_POST["cognome"];
+$destinazione =$_POST["destinazione"];
+$tipo =$_POST["tipo_viaggio"];
+
+$_SESSION["nome"]=$nome;
+$_SESSION["cognome"]=$cognome;
+$_SESSION["destinazione"]=$destinazione;
+$_SESSION["tipo_viaggio"]=$tipo;
+
+if($tipo_viaggio == "affari"){
+    print("Buongiorno $nome $cognome");
+print(Azienda <input type="text" name="nome_azienda">);
+print(Durata_viaggio <input type="text" name="durata_viaggio">);
+print(Budget <input type="text" name="budget">);
+
+
+
+
+
+
+
+}
+?>
+
+</body>
+</html>
