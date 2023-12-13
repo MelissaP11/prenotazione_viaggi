c847f8d892624e08c4f988cb68793a9fbecca841
Melissa <melissa.prencisvalle@iismajoranamoncalieri.edu.it>
2023-12-13 12:56:43 +0100

    modificato

--- dettagli_viaggio.php
+++ dettagli_viaggio.php
@@ -11,20 +11,36 @@ session_start();
 $nome =$_POST["nome"];
 $cognome =$_POST["cognome"];
 $destinazione =$_POST["destinazione"];
-$tipo =$_POST["tipo_viaggio"];
+$tipo_viaggio =$_POST["tipo_viaggio"];
 
 $_SESSION["nome"]=$nome;
 $_SESSION["cognome"]=$cognome;
 $_SESSION["destinazione"]=$destinazione;
-$_SESSION["tipo_viaggio"]=$tipo;
+$_SESSION["tipo_viaggio"]=$tipo_viaggio;
+
+echo("<form method='post' action='riepilogo_prenotazione.php'>");
 
 if($tipo_viaggio == 'affari'){
-    print("Buongiorno $nome $cognome");
-    echo("Azienda : <input type='text' name='nome_azienda'>");
-    echo("Durata_viaggio : <input type='text' name='durata_viaggio'>");
-    echo("Budget :  <input type='text' name='budget'>");
-    
-}else {}
+    print("Buongiorno $nome $cognome <br><br>");
+    echo("Azienda : <input type='text' name='nome_azienda'> <br><br>");
+    echo("Durata viaggio : <input type='number' name='durata_viaggio'> <br><br>");
+    echo("Budget :  <input type='number' name='budget'> <br><br>");
+    echo ("<input type='submit'>");
+
+}else if($tipo_viaggio == 'vacanze') {
+    print("Buongiorno $nome $cognome <br><br>");
+    echo("Numero persone :<input type='number' name='numero_persone'> <br><br>");
+    echo("Tipo Allogio : <input type='text' name='tipo_allogio'> <br><br>");
+    echo ("Inserisci l'attività prescelta: <br>");
+    echo ("<input type='checkbox' name='attivita' value='turismo'>");
+    echo ("<label for='attivita'> Turismo </label><br>");
+    echo ("<input type='checkbox' name='attivita' value='sport'>");
+    echo ("<label for='attivita'> Fare sport </label><br>");
+    echo ("<input type='checkbox' name='attivita' value='musei'>");
+    echo ("<label for='attivita'> Visitare museo </label><br><br>");
+    echo ("<input type='submit'>"); 
+}
+echo ("</form>");
 ?>
 
 
---
+++ riepilogo_prenotazione.php
@@ -0,0 +1,50 @@
+<DOCTYPE html>
+    <html>
+        <head>
+        <title>Riepilogo prenotazione</title> 
+        </head>
+            <body>
+            <h1>Riepilogo prenotazione</h1>
+
+<?php
+session_start();
+$nome = $_SESSION["nome"];
+$cognome = $_SESSION["cognome"];
+$destinazione = $_SESSION["destinazione"];
+$tipo_viaggio = $_SESSION["tipo_viaggio"];
+
+
+
+
+
+
+if($tipo_viaggio == 'affari'){
+    $azienda = $_POST["nome_azienda"];
+    $durata_viaggio = $_POST["durata_viaggio"];
+    $budget = $_POST["budget"];
+    $_SESSION["nome_azienda"]=$azienda;
+    $_SESSION["durata_viaggio"]=$durata_viaggio;
+    $_SESSION["budget"]=$budget;
+    echo("Buongiorno $nome $cognome <br><br> 
+    La tua destinazione è $destinazione e hai scelto 
+    come tipo di viaggio hai scelto: $tipo_viaggio.
+    L'azienda che hai scelto è $azienda e la durata del viaggio sarà $durata_viaggio.
+    Il budget prescelto è $budget.");
+}else if($tipo_viaggio == 'vacanze'){
+    $n_persone = $_POST["numero_persone"];
+    $tipo_allogio = $_POST["tipo_allogio"];
+    $attivita = $_POST["attivita"];
+    $_SESSION["numero_persone"]=$n_persone;
+    $_SESSION["tipo_allogio"]=$tipo_allogio;
+    $_SESSION["attivita"]=$attivita;
+    echo("Buongiorno $nome $cognome <br><br> 
+    La tua destinazione è $destinazione e hai scelto 
+    come tipo di viaggio hai scelto: $tipo_viaggio.
+    Il numero di persone è $n_persone e come allogio hai scelto:$tipo_allogio. 
+    Come attività da svolgere hai scelto: $attivita ");
+}
+
+
+?>
+            </body>
+            </html>
